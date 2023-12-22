8bc66ff73f5014be580f062d81f3d5fb91f0c59a
Howard Kiat <buisnesskiat123@gmail.com>
2023-12-22 12:50:42 +0800

    Bug fixing

--- yumrecipe/src/App.js
+++ yumrecipe/src/App.js
@@ -1,9 +1,10 @@
-
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>title</h1>
+      <Home />
 
     </div>
   );
---
+++ yumrecipe/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Popular from '../components/popular'
+import Veggie from '../components/Veggie'
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+
+    </div>
+  )
+}
+
+export default Home
