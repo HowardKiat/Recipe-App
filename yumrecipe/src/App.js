36b5b83ab7795764774c67f90373d3205f4ac60e
Howard Kiat <buisnesskiat123@gmail.com>
2023-12-26 11:36:53 +0800

    added api fetch

--- yumrecipe/src/App.js
+++ yumrecipe/src/App.js
@@ -1,10 +1,10 @@
-import Home from "./pages/Home";
+import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
       <h1>title</h1>
-      <Home />
+      <Pages />
 
     </div>
   );
