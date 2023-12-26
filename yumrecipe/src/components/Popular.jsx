5faaf66e7038fb24ec40e873b205e513a0c015f7
Howard Kiat <buisnesskiat123@gmail.com>
2023-12-26 17:15:55 +0800

    wow 2hours to realise ' ' to " " fixed the problem

--- yumrecipe/src/components/Popular.jsx
+++ yumrecipe/src/components/Popular.jsx
@@ -1,69 +1,74 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css/splide.min.css';
+import "@splidejs/splide/dist/css/splide.min.css"; 
+// import '../../src/splide-default.min.css';
 
 function Popular() {
-  const [popular, setPopular] =useState([]);
+  const [popular, setPopular] = useState([]);
 
   useEffect(() => {
     getPopular();
-
-  },[]);
+  }, []);
 
   const getPopular = async () => {
     try {
-      const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-      
-      if(!api.ok) {
-        throw new Error('Failed to fetch any data');
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+
+      if (!api.ok) {
+        throw new Error('Failed to fetch data');
       }
-      
+
       const data = await api.json();
       setPopular(data.recipes);
-      setLoading(false);
-
-  } catch (error) {
+    } catch (error) {
       console.error('Error:', error);
-      setLoading(false);
-  }
-};
+      // Handle the error, e.g., show an error message to the user
+    }
+  };
 
-return (
-  <div>
-    <Wrapper>
-      <h3>Popular Picks</h3>
+  // Initialize Splide with some basic settings
+  const splideOptions = {
+    type: 'slide', // Set to 'fade' if you want a fade effect
+    heightRatio: 0.5, // Adjust the slider height as needed
+    pagination: false, // Set to true if you want pagination
+  };
 
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <Splide>
+  return (
+    <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+
+        <Splide options={splideOptions}>
           {popular && popular.map((recipe) => (
             <SplideSlide key={recipe.id}>
               <Card>
                 <p>{recipe.title}</p>
-                <img src={recipe.image} alt={recipe.title} />
+                <img src={recipe.image} alt={recipe.title} onError={(e) => console.error('Image error:', e)} />
               </Card>
             </SplideSlide>
-          ))}
+            ))}
         </Splide>
-      )}
-    </Wrapper>
-  </div>
-);
+      </Wrapper>
+    </div>
+  );
 }
 
 const Wrapper = styled.div`
-margin: 4rem 0rem;
+  margin: 4rem 0rem;
 `;
 
 const Card = styled.div`
-min-height: 25rem;
-border-radius: 2rem;
-overflow: hidden;
-
-img{
+  min-height: 25rem;
   border-radius: 2rem;
-}
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+    max-width: 100%; // Set the maximum width for the image
+    max-height: 15rem; // Set the maximum height for the image
+    object-fit: cover;
+  }
 `;
+
 export default Popular;
