d6cf6a6d607975b728169cf0f2a4b6c7a4ff7e99
Howard Kiat <buisnesskiat123@gmail.com>
2024-01-02 11:29:52 +0800

    restart

--- yumrecipe/src/pages/Cuisine.jsx
+++ yumrecipe/src/pages/Cuisine.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { Link, useParams } from 'react-router-dom';
 
 
 function Cuisine() {
+  const [cuisine,  setCuisine] = useState([]);
 
   const getCuisine = async (name) => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cusine=${name}&number=12`)
-  }
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cusine=${name}&number=12`);
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine('italian')
+  },[]);
 
   return (
     <div>
