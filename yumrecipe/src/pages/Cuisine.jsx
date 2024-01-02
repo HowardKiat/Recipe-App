240582145a1deed0e4069e7321617532036f76e5
Howard Kiat <buisnesskiat123@gmail.com>
2024-01-02 11:38:05 +0800

    Its working dont touch

--- yumrecipe/src/pages/Cuisine.jsx
+++ yumrecipe/src/pages/Cuisine.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { Link, useParams } from 'react-router-dom';
@@ -6,16 +6,22 @@ import { Link, useParams } from 'react-router-dom';
 
 function Cuisine() {
   const [cuisine,  setCuisine] = useState([]);
+  let params = useParams();
 
   const getCuisine = async (name) => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cusine=${name}&number=12`);
-    const recipes = await data.json();
-    setCuisine(recipes.results);
+    try {
+      const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}&number=12`);
+      const recipes = await data.json();
+      setCuisine(recipes.results);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   };
 
   useEffect(() => {
-    getCuisine('italian')
-  },[]);
+    // getCuisine('italian')
+    console.log(params.type);
+  },[params.type]);
 
   return (
     <div>
