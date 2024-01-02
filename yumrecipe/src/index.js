c7da8a1dc9b07cb6f91dcecbe7d1e7f7c86ef658
Howard Kiat <buisnesskiat123@gmail.com>
2024-01-02 10:42:32 +0800

    env file test

--- yumrecipe/src/index.js
+++ yumrecipe/src/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
