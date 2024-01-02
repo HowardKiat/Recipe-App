e85a6186490a5c17dbf0fc9f6fdeae31a5f59448
Howard Kiat <buisnesskiat123@gmail.com>
2024-01-02 10:42:46 +0800

    cuisine in prog

--- yumrecipe/src/pages/Cuisine.jsx
+++ yumrecipe/src/pages/Cuisine.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+
 
 function Cuisine() {
+
+  const getCuisine = async (name) => {
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cusine=${name}&number=12`)
+  }
+
   return (
-    <div>Cuisine</div>
+    <div>
+      
+    </div>
   )
 }
 
