e685abbfd3cda355261ee2ab186e69009391daf4
Howard Kiat <buisnesskiat123@gmail.com>
2024-01-09 11:48:19 +0800

    Initial commit

---
+++ yumrecipe/src/pages/Recipe.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Recipe() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Recipe
