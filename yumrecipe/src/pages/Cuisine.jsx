40cda236427e26f8b183af09d2bc0491bd5116c6
Howard Kiat <buisnesskiat123@gmail.com>
2024-01-10 11:00:27 +0800

    Framer Motion (not important)

--- yumrecipe/src/pages/Cuisine.jsx
+++ yumrecipe/src/pages/Cuisine.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useParams, Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 function Cuisine() {
   const [cuisine, setCuisine] = useState([]);
@@ -40,7 +41,12 @@ function Cuisine() {
   }
 
   return (
-    <Grid>
+    <Grid 
+      animate={{opacity: 1}}
+      initial={{opacity: 0}}
+      exit={{opacity: 0}}
+      transition={{ duration: 0.5}}
+    >
       {cuisine.map((item) => (
         <Card key={item.id}> 
           <Link to={`/recipe/${item.id}`}>
@@ -53,7 +59,7 @@ function Cuisine() {
   );
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
   grid-gap: 3rem;
